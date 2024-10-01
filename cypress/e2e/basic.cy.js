027ae5817cfae655a565f71563118fa231de2a63
23ILYas <151154948+23ILYas@users.noreply.github.com>
2024-10-01 23:12:45 +0500

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,26 +1,18 @@
-
-      var cpm = {
-  onInitialise: function(status) {
-    if (this.hasConsented('analytics')) {
-      console.log('The analytics category is allowed');
-    }
-  },
-  onStatusChange: function(status, previousStatus) {
-    if (this.hasConsented("analytics")) {
-      console.log('The analytics category is allowed');
-    }
-  },
-  onAllow: function(category) {
-    if (category == 'analytics') {
-      console.log('The analytics category was just allowed');
-    }
-  },
-  onRevoke: function(category) {
-    if (category == 'analytics') {
-      console.log('The analytics category was just revoked');
-    }
-  },
-};
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('displays the resources text', () => {
+    cy.get('h2')
+    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
+  })
+  it('renders the image', () => {
+    cy.get('img')
+    .should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0);
+          
+        })
   
-  
-
+      })
+    }) 
